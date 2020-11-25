298ec1959675e526a4bb710723b4c6fa1e83fdbd
Dorjderem2002 <dorjderemdorj202@gmail.com>
2020-11-25 17:31:16 +0800

    Removed duplicate functions from menuEvents.js

--- js/menuEvents.js
+++ js/menuEvents.js
@@ -4,15 +4,3 @@ const path = require("path");
 ipc.on("newfile",function(){
 	createNewFile()
 }) 
-
-ipc.on("newproject",function(){
-	createNewFile()
-}) 
-
-ipc.on("openproject",function(){
-	createNewFile()
-}) 
-
-ipc.on("newfile",function(){
-	createNewFile()
-}) 
